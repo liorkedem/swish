c9c64fa872af48e5ab4981217bef0168cb8745a5
liorkedem <liorke@monday.com>
2021-11-06 16:45:38 +0200

    t

--- src/components/player-page/player-stats-splits/player-stats-splits-component.js
+++ src/components/player-page/player-stats-splits/player-stats-splits-component.js
@@ -1,41 +1,36 @@
 import React from "react";
 import _ from "lodash";
 import "./player-stats-splits-component.css";
+import { ALL_STATS_CATEGORIES } from "../../../constants/stats";
 
-export default class PlayerStatsComponent extends React.Component {
+export default class PlayerStatsSplitsComponent extends React.Component {
   constructor(props) {
     super(props);
   }
 
-  renderCategory(name, value) {
+  renderCell = (value) => {
     return (
       <div className="category-wrapper">
-        <div className="category-name">{name}</div>
         <div className="category-value">{value}</div>
       </div>
     );
-  }
+  };
 
-  renderPeriod(periodStats) {
-    const { period, stats } = periodStats;
+  renderPeriodData = (periodStats) => {
     return (
       <div className="period-wrapper">
-        {period}
-        {Object.entries(stats).map(([key, value]) =>
-          this.renderCategory(key, value)
-        )}
+        {_.map(Object.values(periodStats), this.renderCell)}
       </div>
     );
-  }
+  };
 
   render() {
     const { playerStats } = this.props;
+    const periodsData = Object.values(playerStats);
     return (
       <div className="player-stats-splits-component">
-        {/* {_.map(playerStats, this.renderPeriod)} */}
-        {Object.entries(playerStats).map(([key, value]) =>
-          this.renderPeriod({ period: [key], stats: value })
-        )}
+        {ALL_STATS_CATEGORIES.map(this.renderCell)}
+        {_.map(periodsData, this.renderPeriodData)}
       </div>
     );
   }
